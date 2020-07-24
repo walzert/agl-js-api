478bdf0bdd253f3b015d4f941d6f16642c8fccff
Thomas Walzer <thomas.walzer@reutlingen-university.de>
2020-07-24 14:31:12 +0200

    updated can for writing und subscribing on messages

--- src/low-can.js
+++ src/low-can.js
@@ -1,11 +1,45 @@
 import { call as api_call, subscribe as api_subscribe } from './api';
 
-export function list() {
+export function get_list() {
     return api_call("low-can/list", {});
+}
+
+export function do_auth() {
+    return api_call("low-can/auth", {});
+}
+
+export function do_write(signal_name,signal_value) {
+    return api_call("low-can/write", { "signal_name": signal_name, "signal_value": signal_value});
 }
 
 export function get(event) {
     return api_call("low-can/get", {
-        event: event
+        "event": event
     });
+}
+
+export function subscribe_by_event(event,handler) {
+    return api_subscribe("low-can/subscribe", {
+        "event": event
+    }, handler);
+}
+
+export function unsubscribe_by_event(event,handler) {
+    return api_subscribe("low-can/unsubscribe", {
+        "event": event
+    }, handler);
+}
+
+
+export function subscribe_by_id(id,handler) {
+    return api_subscribe("low-can/subscribe", {
+        "id": id
+    }, handler);
+}
+
+
+export function unsubscribe_by_id(id,handler) {
+    return api_subscribe("low-can/unsubscribe", {
+        "id": id
+    }, handler);
 }
